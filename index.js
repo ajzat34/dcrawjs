80dc73e9ea4cae74dd5623aff8afe6620677fab1
Andrew Johnson <ajzat25@gmail.com>
2020-05-06 01:00:16 -0400

    moved wrapper to this module

--- index.js
+++ index.js
@@ -1,5 +1,24 @@
-var dcraw
-try { dcraw = require('dcrawjs-darwin')} catch(e){}
-try { dcraw = require('dcrawjs-win32')} catch(e){}
+var dcrawpath
+try { dcrawpath = require('dcrawjs-darwin')} catch(e){}
+try { dcrawpath = require('dcrawjs-win32')} catch(e){}
 if (!dcraw) throw new Error('could not load os specific dcraw build, either dependencies are missing, or your os is not supported')
-module.exports = dcraw
+
+module.exports = function(source, options) {
+  return new Promise(function(resolve, reject){
+    try {
+      const sh = spawn(dcrawpath, ['-T','-6','-c',source])
+      let chunks = []
+      sh.stdout.on('data', (chunk) => {
+        chunks.push(chunk)
+      })
+      sh.stderr.on('data', (data) => {
+        console.log(data.toString())
+      })
+      sh.on('close', () => {
+        resolve(Buffer.concat(chunks))
+      })
+    } catch(err){
+      reject(err)
+    }
+  })
+}
