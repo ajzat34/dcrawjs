8b34103cb0e992a91a99d8defb6de1eaef72a389
Andrew Johnson <ajzat25@gmail.com>
2020-05-06 00:24:05 -0400

    bug fix

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var dcraw
-try { dcraw = require('dcrawjs-darwin');} catch(){}
-try { dcraw = require('dcrawjs-win32');} catch(){}
+try { dcraw = require('dcrawjs-darwin')} catch(e){}
+try { dcraw = require('dcrawjs-win32')} catch(e){}
 if (!dcraw) throw new Error('could not load os specific dcraw build, either dependencies are missing, or your os is not supported')
 module.exports = dcraw
